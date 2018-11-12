26af6a11a17ea9ed7c06eef0ce0d6dd3aba4c4da
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2018-11-13 01:10:47 +0200

    wip

--- test/scant_method_test.py
+++ test/scant_method_test.py
@@ -7,10 +7,8 @@ class ScantMethodTest(unittest.TestCase):
     scantObj = ScantMethod()
 
     def test_regular(self):
-        def f(x):
-            return x ** 2 - 20
-
-        self.assertEqual(scantObj.secant(f, 4.5, 0.1, 20), 4.47213595499958)
+        self.assertEqual(scantObj.secant(lambda x: x ** 2 - 20, 4.5, 0.1, 20), 4.47213595499958)
+        self.assertEqual(scantObj.secant(lambda x: x ** 2 - x - 1, 1, 2, 5), 4.47213595499958)
 
 
 if __name__ == '__main__':
