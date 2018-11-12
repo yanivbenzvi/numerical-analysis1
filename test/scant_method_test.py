933915ab02fa08c810d94f7e023a559fafec5303
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2018-11-13 01:16:05 +0200

    wip

--- test/scant_method_test.py
+++ test/scant_method_test.py
@@ -6,8 +6,10 @@ class ScantMethodTest(unittest.TestCase):
     global scantObj
     scantObj = ScantMethod()
 
-    def test_regular(self):
+    def test_exponential_function1(self):
         self.assertEqual(scantObj.secant(lambda x: x ** 2 - 20, 4.5, 0.1, 20), 4.47213595499958)
+
+    def test_exponential_function2(self):
         self.assertEqual(scantObj.secant(lambda x: x ** 2 - x - 1, 1, 2, 5), 1.6180257510729614)
 
 
