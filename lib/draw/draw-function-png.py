771011fdf09594223f69399ae774de2db7dc2000
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2018-11-14 09:19:44 +0200

    add plot draw function

---
+++ lib/draw/draw-function-png.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Create functions and set domain length
+x = np.arange(-5, 5, 0.1)
+y = 2**x
+
+# Plot functions and a point where they intersect
+plt.plot(x, y)
+
+# Config the graph
+plt.title('A Cool Graph')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+
+# Show the graph
+plt.show()
