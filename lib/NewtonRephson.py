1b61d766cb87ca713d2bf3a897c2f87899505e40
shiri rom <sabudaram1@gmail.com>
2018-12-03 10:41:17 +0200

    add prining infromation to some of the method

--- lib/NewtonRephson.py
+++ lib/NewtonRephson.py
@@ -1,9 +1,9 @@
-def NewtonRaphsonMethod(f, f_, x0, TOL=0.001, NMAX=100):
+def NewtonRaphsonMethod(f, f_, x0, TOL=0.001, NMAX=100):#puting f anf f' tolrence,number of itration
     n=1
-    while n<=NMAX:
-        x1 = x0 - (f(x0)/f_(x0))
-        if x1 - x0 < TOL:
+    while n<=NMAX:#while we didnt got to the end continue
+        x1 = x0 - (f(x0)/f_(x0))#first x0 - the func(x0)/f'(x0) and put it in the next x1
+        if x1 - x0 < TOL:#if it closet to the tolorence return the next result of the result
             return x1
-        else:
+        else: #else continue promote the x
             x0 = x1
-    return False
+    return False #if we couldn't find root return fail
--- lib/jacobi_method.py
+++ lib/jacobi_method.py
@@ -3,16 +3,18 @@ from numpy import array, zeros, diag, diagflat, dot
 
 class jacobi_method:
     @staticmethod
-    def jacobi(a, b, n=25, x=None):
+    def jacobi(a, b, n=25, x=None):#gessing a and b
         """Solves the equation Ax=b via the Jacobi iterative method."""
         # Create an initial guess if needed
         if x is None:
             x = zeros(len(a[0]))
-
+            print (x)
         # Create a vector of the diagonal elements of A
         # and subtract them from A
         d = diag(a)
+        print(d)
         r = a - diagflat(d)
+        print(r)
 
         # Iterate for N times
         for i in range(n):
