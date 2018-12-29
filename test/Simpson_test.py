9ff6fb35977b4c92c7814f9da132c84551936220
nizanrosh <31489258+nizanrosh@users.noreply.github.com>
2018-12-29 17:48:46 +0200

    Added Simpsons rule tests.

---
+++ test/Simpson_test.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+from Simpson import simps
+
+
+class SimpsonTest(unittest.TestCase):
+    def test_Simpson_Test(self):
+        self.assertEqual(simps(lambda x : 3*x**2,0,1,10), 1.0)
+
+    def test_Simpson_Test2(self):
+        self.assertEqual(simps(np.sin,0,np.pi/2,100), 1.0000000003382361)
+
+    def test_Simpson_Test3(self):
+        self.assertRaises(ZeroDivisionError, simps(lambda x : x,0,0,0))
+
+
+if __name__ == '__main__':
+    unittest.main()
