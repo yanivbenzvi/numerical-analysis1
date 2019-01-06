21d0ac341249b8e0919647d95ff59bec1654604f
LironGlickman <7LIRONG7@GMAIL.COM>
2019-01-06 22:03:18 +0200

    Neville.py been update

---
+++ lib/Neville.py
@@ -0,0 +1,42 @@
+"""
+A Python program for the Neville's algorithm.
+Contact:
+Ningchuan Xiao
+The Ohio State University
+Columbus, OH
+"""
+
+__author__ = "Ningchuan Xiao <ncxiao@gmail.com>"
+
+def neville(datax, datay, x):
+    """
+    Finds an interpolated value using Neville's algorithm.
+    Input
+      datax: input x's in a list of size n
+      datay: input y's in a list of size n
+      x: the x value used for interpolation
+    Output
+      p[0]: the polynomial of degree n
+    """
+    n = len(datax)
+    m=len(datay)
+    if m!=n :
+        return None
+
+
+
+    p = n*[0]
+    for k in range(n):
+        for i in range(n-k):
+            if k == 0:
+                p[i] = datay[i]
+                print(p[i])
+            else:
+                p[i] = ((x-datax[i+k])*p[i]+ \
+                        (datax[i]-x)*p[i+1])/ \
+                        (datax[i]-datax[i+k])
+                print(p[i])
+    return p[0]
+
+if __name__ == '__main__':
+    print(neville([16, 64, 100], [0.25, 0.125, 0.1], 81))
