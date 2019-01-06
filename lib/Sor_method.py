54aca945d0e2c4e144abd14de7a6eca1c98996ca
Yaniv Ben Zvi <yanivbenzvi023@gmail.com>
2019-01-07 00:07:49 +0200

    wip

--- lib/Sor_method.py
+++ lib/Sor_method.py
@@ -1,267 +1,44 @@
 import numpy as np
 
 
-def iterative(matrix, b, c, printList):
-    result = []
-    sum = 0
-    count = 0
-    k = 0
-    flag = 0
-    flag2 = 0
 
-    for i in range(len(matrix)):
-        result.append(0)
-    while (flag2 == 0 and count <= 100):
-        for i in range(len(matrix)):
-            for j in range(len(matrix)):
-                sum = sum + (matrix.item(i, j) * result[j])
-            sum = sum + c[i]
-            if b[i] > 0.001:
-                sum = sum / b[i]
-            if abs(sum - result[i]) > 0.0000001:
-                flag = 1
-            result[i] = sum
-            sum = 0
+def sor(m, w=1.25, x0=None, eps=1e-5, max_iteration=100):
+    """
+    Parameters
+    ----------
+    m  : list of list of floats : coefficient matrix
+    w  : float : weight
+    x0 : list of floats : initial guess
+    eps: float : error tolerance
+    max_iteration: int
 
-        if flag == 0:
-            flag2 = 1
-        else:
-            flag = 0
-        count = count + 1
-        printList.append(count)
-        for j in result:
-            printList.append(j)
+    Returns
+    -------
+    list of floats
+        solution to the system of linear equation
 
-    return result
+    Raises
+    ------
+    ValueError
+        Solution does not converge
+    """
+    n = len(m)
+    x0 = [0] * n if x0 == None else x0
+    x1 = x0[:]
 
-
-def is_close(line, b, result, eps):
-    sum = 0
-    for i in range(line.getA().size):
-        sum = sum + (result[i] * line.item(0, i))
-    if abs(sum - b) < eps:
-        return True
-    return False
+    for __ in range(max_iteration):
+        for i in range(n):
+            s = sum(-m[i][j] * x1[j] for j in range(n) if i != j)
+            x1[i] = w * (m[i][n] + s) / m[i][i] + (1 - w) * x0[i]
+        if all(abs(x1[i] - x0[i]) < eps for i in range(n)):
+            return x1
+        x0 = x1[:]
+    raise ValueError('Solution does not converge')
 
 
-def sum_m(mat1, mat2):
-    result = []
+if __name__ == '__main__':
+    m = [[4, 3.2, 0.5, 9.2], [2.2, 3, -0.3, 0.9], [-3.1, -0.2, 4, 7]]
+    print(sor(m))
 
-    for i in range(len(mat1)):
-        mylist = []
-        for j in range(len(mat2)):
-            mylist.append(0.0)
-        result.append(mylist)
-
-    mat = np.matrix(result)
-
-    for i in range(len(mat1)):
-        for j in range(len(mat2)):
-            mat.itemset((i, j), (mat1.item(i, j) + mat2.item(i, j)))
-
-    new_mat = np.matrix(mat)
-    return new_mat
-
-
-def multi_m(mat1, mat2):
-    result = []
-
-    for i in range(len(mat1)):
-        mylist = []
-        for j in range(len(mat1)):
-            mylist.append(0.0)
-        result.append(mylist)
-
-    mat = np.matrix(result)
-
-    for i in range(len(mat1)):
-        for j in range(len(mat1)):
-            for k in range(len(mat1)):
-                mat.itemset((i, j), (mat1.item(i, k) + mat2.item(k, j)))
-
-    new_mat = np.matrix(mat)
-    return new_mat
-
-
-def multi_scalar(mat, num):
-    result = []
-
-    for i in range(len(mat)):
-        mylist = []
-        for j in range(len(mat)):
-            mylist.append(mat.item(i, j) * num)
-        result.append(mylist)
-
-    new_mat = np.matrix(result)
-    return new_mat
-
-
-def create_D(matrix):
-    result = []
-    for i in range(len(matrix)):
-        m = []
-        for j in range(len(matrix)):
-            if i == j:
-                m.append(float(matrix.item(i, j)))
-            else:
-                m.append(0.0)
-        result.append(m)
-    D = np.matrix(result)
-    return D
-
-
-def create_L(matrix):
-    result = []
-    for i in range(len(matrix)):
-        m = []
-        for j in range(len(matrix)):
-            if i > j:
-                m.append(float(matrix.item(i, j)))
-            else:
-                m.append(0.0)
-        result.append(m)
-    L = np.matrix(result)
-    return L
-
-
-def create_U(matrix):
-    result = []
-    for i in range(len(matrix)):
-        m = []
-        for j in range(len(matrix)):
-            if i < j:
-                m.append(float(matrix.item(i, j)))
-            else:
-                m.append(0.0)
-        result.append(m)
-    U = np.matrix(result)
-    return U
-
-
-def PrintList(printList, size):
-    for i in range(0, len(printList) - 1, size):
-        print("number iteration:", printList[i])
-        for j in range(1, size):
-            print("x", j, ": ", printList[i + j])
-
-
-def SOR(matrix, b):
-    emptyL = [0 for i in b]
-    flag = 0
-    w = 0.05
-    D = create_D(matrix)
-    L = create_L(matrix)
-    U = create_U(matrix)
-    while flag == 0 and w <= 2:
-        printList = []
-
-        W_L = multi_scalar(L, w)
-        D_WL = sum_m(W_L, D)
-        W_D = multi_scalar(D, 1 - w)
-        W_U = multi_scalar(U, w)
-
-        new_b = []
-        for i in range(len(b)):
-            new_b.append(b[i] * w)
-        result = []
-        for i in range(len(matrix)):
-            m = []
-            for j in range(0, i):
-                m.append(-D_WL.item(i, j))
-            m.append(W_D.item(i, i))
-            for k in range(i + 1, len(matrix)):
-                m.append(-W_U.item(i, k))
-            result.append(m)
-        mat = np.matrix(result)
-
-        vector = []
-        for i in range(len(matrix)):
-            vector.append(D.item(i, i))
-
-        r = iterative(mat, vector, new_b, printList)
-        close = True
-        for i in range(len(matrix)):
-            if is_close(matrix[i], b[i], r, 0.0001) == False:
-                close = False
-
-        if close == False:
-            w = w + 0.05
-        else:
-            flag = 1
-            print("SOR Method")
-            print("Convergence is carried out for w = ", w)
-            print("The calculate of the Vector results is running by the formula - (D+wL)Xr+1=[(1-w)D-wU]Xr+wb")
-            print("The initial guess is - ", emptyL)
-            print("Matrix D\n", D)
-            print("Matrix L\n", L)
-            print("Matrix U\n", U)
-            print("Matrix D+wL\n", D_WL)
-            print("Matrix (1-w)D\n", W_D)
-            print("Matrix wU\n", W_U)
-            print("Vector wb\n", new_b)
-            print("The final Matrix\n", mat)
-            PrintList(printList, len(r) + 1)
-            print("The Vector results is - ")
-    return r
-
-
-def StrongcalcDominant(mat):
-    mat = np.matrix(mat)
-    newOrder = list(range(len(mat)))
-    found = False
-    for n in range(len(mat)):
-        for m in range(n, len(mat)):
-            if (abs(mat[n, m]) > abs(np.sum(mat[n, :]) - mat[n, m])):
-                newOrder.pop(m)
-                newOrder.insert(n, m)
-                found = True
-        if not found:
-            return None
-        mat = mat[:, newOrder]
-        newOrder = list(range(len(mat)))
-        found = False
-    return mat
-
-
-def calcDominant(mat, b):
-    mat = np.matrix(mat)
-    newOrder = list(range(len(mat)))
-    found = False
-    for m in range(len(mat)):
-        c = []
-        for n in range(m, len(mat)):
-            if (abs(mat[n, m]) >= abs(np.sum(mat[n:, m]) - mat[n, m])):
-                newOrder.pop(n)
-                newOrder.insert(m, n)
-                found = True
-                break
-        if not found:
-            return None
-        mat = mat[newOrder, :]
-        for i in range(len(b)):
-            c.append(b[newOrder[i]])
-        b = c
-        newOrder = list(range(len(mat)))
-        found = False
-    return mat, b
-
-
-def dominant(mat, b):
-    new_mat = StrongcalcDominant(mat)
-    if type(new_mat) == type(None):
-        new_mat = calcDominant(mat, b)
-        if new_mat != None:
-            return new_mat
-    else:
-        return new_mat, b
-    return mat, b
-
-
-if __name__ == "__main__":
-    mat = np.matrix([[1, 3, 5], [1, 4, 2], [5, 2, 9]])
-    b = [1, 2, 3]
-
-    r = dominant(mat, b)
-    mat = r[0]
-    b = r[1]
-    print(SOR(mat, b))
+# author: Worasait Suwannik
+# date: May 2015
--- lib/jacobi_method.py
+++ lib/jacobi_method.py
@@ -1,25 +1,75 @@
 from numpy import array, zeros, diag, diagflat, dot
 
+def create_D(matrix):
+    result = []
+    for i in range(len(matrix)):
+        m = []
+        for j in range(len(matrix)):
+            if i == j:
+                m.append(float(matrix.item(i, j)))
+            else:
+                m.append(0.0)
+        result.append(m)
+    D = np.matrix(result)
+    return D
 
-class jacobi_method:
-    @staticmethod
-    def jacobi(a, b, n=25, x=None):#gessing a and b
-        """Solves the equation Ax=b via the Jacobi iterative method."""
-        # Create an initial guess if needed
-        if x is None:
-            x = zeros(len(a[0]))
-            print (x)
-        # Create a vector of the diagonal elements of A
-        # and subtract them from A
-        d = diag(a)
-        print(d)
-        r = a - diagflat(d)
-        print(r)
+def create_L(matrix):
+    result = []
+    for i in range(len(matrix)):
+        m = []
+        for j in range(len(matrix)):
+            if i > j:
+                m.append(float(matrix.item(i, j)))
+            else:
+                m.append(0.0)
+        result.append(m)
+    L = np.matrix(result)
+    return L
 
-        # Iterate for N times
-        for i in range(n):
-            x = (b - dot(r, x)) / d
-        return x
+def create_U(matrix):
+    result = []
+    for i in range(len(matrix)):
+        m = []
+        for j in range(len(matrix)):
+            if i < j:
+                m.append(float(matrix.item(i, j)))
+            else:
+                m.append(0.0)
+        result.append(m)
+    U = np.matrix(result)
+    return U
+
+def jacobi(matrix, b):
+    D = create_D(matrix)
+    L = create_L(matrix)
+    U = create_U(matrix)
+
+    D_1 = D.I
+    D_minus_1 = multi_scalar(D_1, -1)
+    L_U = sum_m(L, U)
+    L_U_D_1 = np.matmul(D_minus_1, L_U)
+    L_D_b = np.matmul(D_1, b)
+
+    new_b = []
+    for i in range(len(b)):
+        new_b.append(1.0)
+
+    L_D_b = L_D_b.getA()[0]
+
+    printList = []
+    print("Jaccobi Method")
+    print("The calculate of the Vector results is running by the formula - Xr+1=-D^-1(L+U)Xr+D^-1*b")
+    r = iterative(L_U_D_1, new_b ,L_D_b, printList)
+    print("Matrix D\n", D)
+    print("Matrix L\n", L)
+    print("Matrix U\n", U)
+    print("Matrix -D^-1\n", D_minus_1)
+    print("Matrix L+U\n", L_U)
+    print("Matrix -D^-1*(L+U)\n", L_U_D_1)
+    print("Vector D^-1*b\n", L_D_b)
+    PrintList(printList, len(r) + 1)
+    print("The Vector results is - ")
+    return r
 
 
 '''https://gist.github.com/angellicacardozo/3a0891adfa38e2c4187612e57bf271d1'''
