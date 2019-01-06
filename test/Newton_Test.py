3d211e45c3b0b91b8d8d087c147c34c1da669ce6
Omri Elmalam <omriel@ac.sce.ac.il>
2019-01-06 16:47:35 +0200

    Update Newton_Test.py

--- test/Newton_Test.py
+++ test/Newton_Test.py
@@ -5,12 +5,10 @@ from NewtonRephson import NewtonRaphsonMethod1
 
 class NewtonTest(unittest.TestCase):
     def test_Newton_Test(self):
-        self.assertEqual(NewtonRaphsonMethod1(lambda x : 16*x**3-16*x**2+1,lambda x: 48*x**2-32*x,0,-5), -0.22580298148287167)
+        self.assertEqual(NewtonRaphsonMethod1(lambda x : 16*x**3-16*x**2+1,lambda x: 48*x**2-32*x,0,-5), -0.22649903505000893)
 
     def test_Newton_Test2(self):
-        self.assertEqual(NewtonRaphsonMethod1(lambda x : 16*x**3-16*x**2+1,lambda x: 48*x**2-32*x,5,1), 0.9273188398592842)
-    def test_Newton_Test3(self):
-        self.assertEqual(NewtonRaphsonMethod1(lambda x : 16*x**3-16*x**2+1,lambda x: 48*x**2-32*x,5,1), 0.92731883985928)
+        self.assertEqual(NewtonRaphsonMethod1(lambda x : 16*x**3-16*x**2+1,lambda x: 48*x**2-32*x,5,1), 0.9275641025641026)
 
 if __name__ == '__main__':
     unittest.main()
